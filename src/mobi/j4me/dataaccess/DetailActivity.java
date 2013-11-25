e057aae2d7ca010623cd886755f382134d894bf3
Pedro Lopez <plp750@gmail.com>
2013-11-25 20:36:10 +0100

    Week02 Monday

--- src/mobi/j4me/dataaccess/DetailActivity.java
+++ src/mobi/j4me/dataaccess/DetailActivity.java
@@ -3,42 +3,40 @@ package mobi.j4me.dataaccess;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 
-public class WelcomeActivity extends Activity {
+public class DetailActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.welcome);
+		setContentView(R.layout.detail);
 		Intent intent = getIntent();
-		
-		String userID = intent.getStringExtra("userID");
+				
+		String task = intent.getStringExtra("task");
 		
 		EditText edT = (EditText) findViewById( R.id.editText1);
-		edT.setText( userID);
+		edT.setText( task);
 		
-		//Get the id of the button
-		
-		Button btn = (Button) findViewById( R.id.btn_submit);
+		Button btn = (Button) findViewById( R.id.btn_send_message_id);
 		btn.setOnClickListener( new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				
 				
-				Spinner sp = (Spinner) findViewById( R.id.sp_category_id);
-				String selectedTask = (String) sp.getSelectedItem();
+				String phoneNumber 	= ((EditText) findViewById( R.id.et_phone_number_id)).getText().toString();
+				String smsMessage	= ((EditText) findViewById( R.id.et_message_id)).getText().toString();
+				
+				SmsManager smsMng = SmsManager.getDefault();
+				smsMng.sendTextMessage( phoneNumber, null, smsMessage, null, null);
 				
 				
-				Intent intentWelcome = new Intent(  WelcomeActivity.this, DetailActivity.class);
-				intentWelcome.putExtra("task", selectedTask);
-				startActivity(intentWelcome);
 			}
 		});
 		
